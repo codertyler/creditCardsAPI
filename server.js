4ea5ad511a3f7643f26021f48081eb72f0354b11
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-01-30 23:28:59 -0700

    added get

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
   if (req.method === 'POST' || req.method === 'DELETE' || req.method === 'PUT' || req.method === 'PATCH') {
-    res.send("Not allowed!")
+    return
   }
   // Continue to JSON Server router
   next()
