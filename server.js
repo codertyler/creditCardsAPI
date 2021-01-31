2214fd6052758e044128b2817c84c4bae627dc5a
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-01-30 23:11:58 -0700

    only GET method allowed

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const router = jsonServer.router('banks.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
-
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET');
   
 })
+
+server.use(middlewares);
+server.use(router);
+
+
 
 server.listen(port);
